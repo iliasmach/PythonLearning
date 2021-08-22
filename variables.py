7812704ad166ce6d7c1a2e01bd5573ead1d0686e
Ilias Makhiyanov <iliasmach@cloud-mind.com>
2021-08-22 14:00:29 +0300

    Init

---
+++ variables.py
@@ -0,0 +1,16 @@
+string = "Hello string"
+integer = 321
+float_var = 3.551
+is_human = True
+is_orc = False
+
+print(string, integer, float_var, is_human, is_orc)
+
+complex = 2 + 1j
+
+complex2 = 3 - 3j
+
+print(type(complex))
+print(complex)
+
+print(complex + complex2)
